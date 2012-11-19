81303276630d50b271935c64160f6e5f0e0784f3
David Yoon <dave.yoon@gmail.com>
2012-11-18 19:19:52 -0800

    added a page that lists the payments that you have made and received

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,10 @@
 class PaymentsController < ApplicationController
 
+  def index 
+    @payments_sent = @current_user.payments_sent
+    @payments_received = @current_user.payments_received
+  end
+  
   def create 
     @payment = Payment.create(params[:payment])
     redirect_to :root
