ee88f5eb4333574d89643b02d4757e99fbe1d8f1
Skylar Woodward <skylar@kiva.org>
2012-11-18 19:51:31 -0800

    layout cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,25 @@ class User < ActiveRecord::Base
   
   has_many :payments_sent, :class_name => Payment, :foreign_key => :sender_id
   has_many :payments_received, :class_name => Payment, :foreign_key => :recipient_id
+
+  def has_credit_card
+  	unless !cc_number.nil? && cc_number.length > 10 && 
+      !cc_month.nil? && cc_month.length > 0 &&
+      !cc_year.nil? && cc_year.length == 4
+  		
+      false
+  	else
+  		true
+  	end
+  end
+
+  def has_bank_account
+  	unless !bank_account.nil? && bank_account.length > 7 && 
+      !bank_routing.nil? && bank_routing.length > 7
+
+  		false
+  	else
+  		true
+  	end
+  end
 end
